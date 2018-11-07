484817cc49b327c175c71aab08de1b0c9b3995f3
Yorick Smilda <y.smilda@gmail.com>
2018-11-07 14:15:42 +0100

    added check for line ending

--- MC34063 calculator.go
+++ MC34063 calculator.go
@@ -13,46 +13,52 @@ var vin, vinperc, vinmin, vout, ripple, i, vf, vsat, frequency float64
 
 func main() {
 	retry := true
+	var check string
 
 	for retry == true {
 		reader := bufio.NewReader(os.Stdin)
 
 		fmt.Print("Enter Vin (V): ")
 		input, _ := reader.ReadString('\n')
-		input = strings.TrimSuffix(input, "\r\n")
+		if strings.HasSuffix(input, "\r\n") {
+			check = "\r\n"
+		} else {
+			check = "\n"
+		}
+		input = strings.TrimSuffix(input, check)
 		vin, _ = strconv.ParseFloat(input, 64)
 
 		fmt.Print("Enter Vin delta (%): ")
 		input, _ = reader.ReadString('\n')
-		input = strings.TrimSuffix(input, "\r\n")
+		input = strings.TrimSuffix(input, check)
 		vinperc, _ = strconv.ParseFloat(input, 64)
 
 		fmt.Print("Enter Vout (V): ")
 		input, _ = reader.ReadString('\n')
-		input = strings.TrimSuffix(input, "\r\n")
+		input = strings.TrimSuffix(input, check)
 		vout, _ = strconv.ParseFloat(input, 64)
 
 		fmt.Print("Enter ripple (mV): ")
 		input, _ = reader.ReadString('\n')
-		input = strings.TrimSuffix(input, "\r\n")
+		input = strings.TrimSuffix(input, check)
 		ripple, _ = strconv.ParseFloat(input, 64)
 		ripple /= 1000
 
 		fmt.Print("Enter Iout (mA): ")
 		input, _ = reader.ReadString('\n')
-		input = strings.TrimSuffix(input, "\r\n")
+		input = strings.TrimSuffix(input, check)
 		i, _ = strconv.ParseFloat(input, 64)
 		i /= 1000
 
 		fmt.Print("Enter frequency (KHz): ")
 		input, _ = reader.ReadString('\n')
-		input = strings.TrimSuffix(input, "\r\n")
+		input = strings.TrimSuffix(input, check)
 		frequency, _ = strconv.ParseFloat(input, 64)
 		frequency *= 1000
 
 		fmt.Print("Enter forward voltage diode (V): ")
 		input, _ = reader.ReadString('\n')
-		input = strings.TrimSuffix(input, "\r\n")
+		input = strings.TrimSuffix(input, check)
 		vf, _ = strconv.ParseFloat(input, 64)
 
 		vinmin = (vin - (vin * (vinperc / 100)))
@@ -75,7 +81,7 @@ func main() {
 			fmt.Println()
 			fmt.Print("Want to do another calculation? (Y/N)")
 			input, _ = reader.ReadString('\n')
-			input = strings.TrimSuffix(input, "\r\n")
+			input = strings.TrimSuffix(input, check)
 			fmt.Println()
 			if (input == "y") || (input == "Y") {
 				retry = true
